af769933bf136f83759f499916708b64e8601d4a
LaiaRuizM <laia.ruizm@hotmail.com>
2023-06-07 12:58:56 +0200

    feat: add functionality to NoteList.js

--- src/components/dashboardNotes/NoteList.js
+++ src/components/dashboardNotes/NoteList.js
@@ -1,6 +1,13 @@
 import NoteItem from "./NoteItem";
 
-const NoteList = ({ allNotes, newNote, allCategory }) => {
+const NoteList = ({
+  allNotes,
+  newNote,
+  allCategory,
+  deleteAllNotes,
+  handleDeleteNote,
+  handleEditNote,
+}) => {
   const eachNote = allNotes.map((note, index) => {
     return (
       <NoteItem
@@ -12,14 +19,20 @@ const NoteList = ({ allNotes, newNote, allCategory }) => {
         newNote={newNote}
         allCategory={allCategory}
         categoryTheme={note.categoryTheme}
+        handleDeleteNote={handleDeleteNote}
+        handleEditNote={handleEditNote}
       ></NoteItem>
     );
   });
   return (
     <section>
-      <div>
-        <button>Delete notes</button>
-      </div>
+      {allNotes.length !== 0 ? (
+        <div>
+          <button onClick={deleteAllNotes}>Delete all notes</button>
+        </div>
+      ) : (
+        ""
+      )}
       <ul>{eachNote}</ul>
     </section>
   );

51a80f0652eb6bcea42b07da3fde2d811fd42d67
LaiaRuizM <laia.ruizm@hotmail.com>
2023-06-07 11:12:33 +0200

    feat: add NoteList component

---
+++ src/components/dashboardNotes/NoteItem.js
@@ -0,0 +1,5 @@
+const NoteItem = ({ allNotes, newNote, allCategory }) => {
+  return <h1>Prove</h1>;
+};
+
+export default NoteItem;
--- src/components/dashboardNotes/NoteList.js
+++ src/components/dashboardNotes/NoteList.js
@@ -1,5 +1,28 @@
+import NoteItem from "./NoteItem";
+
 const NoteList = ({ allNotes, newNote, allCategory }) => {
-  return <h1>Prove</h1>;
+  const eachNote = allNotes.map((note, index) => {
+    return (
+      <NoteItem
+        index={index}
+        id={note.id}
+        key={note.id + note.title}
+        title={note.title}
+        content={note.title}
+        newNote={newNote}
+        allCategory={allCategory}
+        categoryTheme={note.categoryTheme}
+      ></NoteItem>
+    );
+  });
+  return (
+    <section>
+      <div>
+        <button>Delete notes</button>
+      </div>
+      <ul>{eachNote}</ul>
+    </section>
+  );
 };
 
 export default NoteList;

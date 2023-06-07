7787e830222eb23453ab94edd5ff3ae083904f9e
LaiaRuizM <laia.ruizm@hotmail.com>
2023-06-07 11:19:41 +0200

    feat: add NoteItem component

--- src/components/dashboardNotes/NoteItem.js
+++ src/components/dashboardNotes/NoteItem.js
@@ -1,5 +1,33 @@
-const NoteItem = ({ allNotes, newNote, allCategory }) => {
-  return <h1>Prove</h1>;
+import { useState } from "react";
+
+const NoteItem = ({
+  allNotes,
+  newNote,
+  allCategory,
+  index,
+  id,
+  title,
+  content,
+  categoryTheme,
+}) => {
+  const [colorNote, setColorNote] = useState("#86E1E0");
+  return (
+    <li>
+      List
+      <div>
+        <div>
+          <small>Category</small>
+          <h3>Title</h3>
+          <p>Content</p>
+        </div>
+        <span></span>
+        <span>
+          <i>icon edit-pencil</i>
+          <i>icon trash</i>
+        </span>
+      </div>
+    </li>
+  );
 };
 
 export default NoteItem;

8e146660e5f56e932b794141bd3123b4461548ff
LaiaRuizM <laia.ruizm@hotmail.com>
2023-06-09 17:07:59 +0200

    feat: add style to Aside

--- src/components/createYourNote/Aside.js
+++ src/components/createYourNote/Aside.js
@@ -12,14 +12,19 @@ const Aside = ({
 }) => {
   return (
     <aside className="aside">
-      <PostedNote
-        handleCreateNewNote={handleCreateNewNote}
-        newNote={newNote}
-        allCategory={allCategory}
-        noteIsEditing={noteIsEditing}
-        resetPostedNote={resetPostedNote}
-        handleUpdateNote={handleUpdateNote}
-        handleInputNote={handleInputNote}></PostedNote>
+      <div className="aside__header">
+        <h2 className="aside__header__title">Notes</h2>
+      </div>
+      <div className="aside__content">
+        <PostedNote
+          handleCreateNewNote={handleCreateNewNote}
+          newNote={newNote}
+          allCategory={allCategory}
+          noteIsEditing={noteIsEditing}
+          resetPostedNote={resetPostedNote}
+          handleUpdateNote={handleUpdateNote}
+          handleInputNote={handleInputNote}></PostedNote>
+      </div>
     </aside>
   );
 };
